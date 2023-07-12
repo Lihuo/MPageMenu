c6d35d6546e2baa41f8068c8217a53678a8ed4ab
Lihour <lihou.kong@lyhourpay.com>
2023-07-12 14:59:14 +0700

    Initial commit

---
+++ Example/MPageMenu/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  MPageMenu
+//
+//  Created by Lihour on 07/12/2023.
+//  Copyright (c) 2023 Lihour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
