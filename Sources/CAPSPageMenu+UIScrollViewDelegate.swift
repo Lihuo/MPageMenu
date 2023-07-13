5955e97d99849f1bb7f9aee917dd30e109cf9df2
Lihuo <konglihuo@gmail.com>
2023-07-13 13:39:10 +0700

    Fix issue scrollview not update

--- Sources/CAPSPageMenu+UIScrollViewDelegate.swift
+++ Sources/CAPSPageMenu+UIScrollViewDelegate.swift
@@ -14,7 +14,7 @@ extension CAPSPageMenu : UIScrollViewDelegate {
         if !didLayoutSubviewsAfterRotation {
             if scrollView.isEqual(controllerScrollView) {
                 if scrollView.contentOffset.x >= 0.0 && scrollView.contentOffset.x <= (CGFloat(controllerArray.count - 1) * self.view.frame.width) {
-                    if (currentOrientationIsPortrait && UIApplication.shared.statusBarOrientation.isPortrait) || (!currentOrientationIsPortrait && UIApplication.shared.statusBarOrientation.isLandscape) {
+                    //if (currentOrientationIsPortrait && UIApplication.shared.statusBarOrientation.isPortrait) || (!currentOrientationIsPortrait && UIApplication.shared.statusBarOrientation.isLandscape) {
                         // Check if scroll direction changed
                         if !didTapMenuItemToScroll {
                             if didScrollAlready {
@@ -136,7 +136,7 @@ extension CAPSPageMenu : UIScrollViewDelegate {
                         menuScrollView.setContentOffset(offset, animated: false)
                     }
                 }
-            }
+            //}
         } else {
             didLayoutSubviewsAfterRotation = false
             
