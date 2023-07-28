3721dc13a4eb2c5c514487c98d9efa1f3e19e7f7
Lihour <lihou.kong@lyhourpay.com>
2023-07-28 09:47:39 +0700

    update xcode files

---
+++ MPageMenu/MPageMenu/MPageMenuViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MPageMenuViewController.swift
+//  MPageMenu
+//
+//  Created by Lihour on 28/7/23.
+//
+
+import Foundation
+import UIKit
+
+
+class MPageMenu: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
