0f8f035364af744a870cf234af6dc43cfe7fc7a3
Lihour <lihou.kong@lyhourpay.com>
2023-07-28 11:06:55 +0700

    resolve conflict update xcode files

---
+++ MPageMenu/MPageMenu/MPageMenu.h
@@ -0,0 +1,18 @@
+//
+//  MPageMenu.h
+//  MPageMenu
+//
+//  Created by Lihour on 28/7/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MPageMenu.
+FOUNDATION_EXPORT double MPageMenuVersionNumber;
+
+//! Project version string for MPageMenu.
+FOUNDATION_EXPORT const unsigned char MPageMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MPageMenu/PublicHeader.h>
+
+
